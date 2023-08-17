da341bff3e72bd399e6357d4d6d8a97444424521
Ivan Konovskoy <konovskoy.i@gmail.com>
2023-08-17 16:23:08 +0700

    set up laravel-mix, install dependencies and write test js code for visualisation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,37 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+
+// if (document.getElementById('plus')) {
+//     value = document.getElementById('plus').value + 1;
+// }
+//
+// if (document.getElementById('minus')) {
+//
+// }
+//
+// let container = document.getElementById('container');
+//
+// function changeQuantity(e)
+// {
+//     let value;
+//
+//     if (e.target.classList.contains('plus')) {
+//         value = document.getElementById('plus').value + 1;
+//     } else if (e.target.classList.contains('minus')) {
+//         value = document.getElementById('minus').value - 1;
+//     } else if (e.target.classList.contains('change-quantity-input')) {
+//         value = document.getElementById('change-quantity-input').value;
+//     }
+//
+//     let data = {
+//       'productId': 5,
+//       'quantity': value
+//     };
+//
+//     xhr.post('/cart/updateQuantity', data);
+// }
+//
+// document.addEventListener('click', changeQuantity);
+// document.addEventListener('input', changeQuantity);
